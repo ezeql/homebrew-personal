c0ab161be172e390eac8ff13c2de96d22758d3b8
Ezequiel Moreno <319609+ezeql@users.noreply.github.com>
2025-01-22 16:50:18 +0100

    Update oabtray to v0.1.9

--- Formula/oabtray.rb
+++ Formula/oabtray.rb
@@ -1,8 +1,8 @@
 class Oabtray < Formula
   desc "Bitcoin price tracker for macOS menu bar with ALABA_FACTOR"
   homepage "https://github.com/ezeql/oabtray"
-  url "https://github.com/ezeql/oabtray/archive/refs/tags/v0.1.8.tar.gz"
-  sha256 "e8eae641af41d1dd4a7c39c78281b0712fa69656482cc3f735f6065eb162e277"
+  url "https://github.com/ezeql/oabtray/archive/refs/tags/v0.1.9.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   depends_on "go" => :build
