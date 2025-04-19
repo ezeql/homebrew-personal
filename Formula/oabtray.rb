4b0b2f8f6b28a77bc256847661387d3d12e869af
ezeql <ezeqle@gmail.com>
2025-04-19 12:27:59 -0300

    Update oabtray to vv0.1.10

--- Formula/oabtray.rb
+++ Formula/oabtray.rb
@@ -1,8 +1,8 @@
 class Oabtray < Formula
   desc "Bitcoin price tracker for macOS menu bar with ALABA_FACTOR"
   homepage "https://github.com/ezeql/oabtray"
-  url "https://github.com/ezeql/oabtray/archive/refs/tags/v0.1.9.tar.gz"
-  sha256 "8f80480352ed7f6b20832c7189282b7ae30cfa05c1c83c5f1c046841e123f111"
+  url "https://github.com/ezeql/oabtray/archive/refs/tags/vv0.1.10.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   depends_on "go" => :build
@@ -11,7 +11,7 @@ class Oabtray < Formula
     mkdir_p bin
     system "go", "build",
            "-ldflags=-s -w " +
-           "-X main.VERSION=v0.1.9",
+           "-X main.VERSION=vv0.1.10",
            "-o", bin/"oabtray"
   end
 
